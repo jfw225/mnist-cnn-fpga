475fd5040d0dafcfbdf445c2395d80b07244505d
jfw225 <jfw225@cornell.edu>
2022-05-13 20:55:10 -0400

    started explaining the layers

--- report/src/App.tsx
+++ report/src/App.tsx
@@ -5,29 +5,13 @@ import Latex from "react-latex";
 
 import ReportFile from "./ReportFile";
 
-// import report from "./src/report-jup.html";
-async function loadReportFile(setHtmlFile: any) {
-	// fetch the report file
-	const file = await fetch("./public/report-jup.html");
-	console.log(file)
-
-	// convert the report file to a string
-	const file_str = await file.text();
-
-	console.log(file_str);
-	// set the report element
-	setHtmlFile({ __html: file_str });
-}
 
 function App() {
 	// const [htmlFile, setHtmlFile] = useState({ __html: "<div> <h3> hi </h3> </div>" })
 	const [htmlFile, setHtmlFile] = useState({ __html: ReportFile })
 
 	useEffect(() => {
-		// loadReportFile(setHtmlFile);
 	}, [])
-
-	console.log(htmlFile)
 
 
 	return <div dangerouslySetInnerHTML={htmlFile} ></div>
